65af91562a8de0eb1d20859a48c2bcc601ea28f5
Simon-Olivier Lachance <49047004+monstrotac@users.noreply.github.com>
2023-04-21 17:00:46 -0400

    Create DndCharacterCreation.js

---
+++ src/routes/WikiDir/DndCharacterCreation.js
@@ -0,0 +1,18 @@
+
+import "../../assets/css/styles.css"
+import { CreateWikiHeader } from "../Wiki"
+
+
+
+function DnDCharacterCreation() {
+    return (
+        <div class="Container">
+            {CreateWikiHeader()}
+            <div className="d-flex flex-column p-5 mb-5 justify-content-center align-items-center">
+                <h1 className="mb-3">Section underwork</h1>
+            </div>
+        </div>
+    );
+}
+
+export default DnDCharacterCreation;
