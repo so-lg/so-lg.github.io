df36519b1d174f3e0396622ed5e19d41ef38ac5e
Simon-Olivier Lachance <49047004+monstrotac@users.noreply.github.com>
2023-04-21 17:00:50 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './Logo.png';
 import './App.css';
 import { Routes, Route} from "react-router-dom";
 import About from "./routes/About";
 import Wiki from "./routes/Wiki";
 import Home from "./routes/Home";
+import UniverseOrigin from "./routes/WikiDir/UniverseOrigin";
+import DndCharacterCreation from "./routes/WikiDir/DndCharacterCreation";
+import UinderalCharactersFactions from "./routes/WikiDir/UinderalCharactersFactions";
+import UinderalCreaturesRacesEntities from "./routes/WikiDir/UinderalCreaturesRacesEntities";
+import UinderalEnergiesItems from "./routes/WikiDir/UinderalEnergiesItems";
+import UniverseDimensions from "./routes/WikiDir/UniverseDimensions";
+import UniverseHistory from "./routes/WikiDir/UniverseHistory";
+import UniversePlanes from "./routes/WikiDir/UniversePlanes";
 import NavigationBar from './Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,6 +21,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/wiki" element={<Wiki />} />
+        <Route path="/wiki/UniverseOrigin" element={<UniverseOrigin />} />
+        <Route path="/wiki/UniverseDimensions" element={<UniverseDimensions />} />
+        <Route path="/wiki/UniversePlanes" element={<UniversePlanes />} />
+        <Route path="/wiki/UinderalEnergiesItems" element={<UinderalEnergiesItems />} />
+        <Route path="/wiki/UinderalCharactersFactions" element={<UinderalCharactersFactions />} />
+        <Route path="/wiki/UinderalCreaturesRacesEntities" element={<UinderalCreaturesRacesEntities />} />
+        <Route path="/wiki/DnDCharacterCreation" element={<DndCharacterCreation />} />
+        <Route path="/wiki/UniverseHistory" element={<UniverseHistory />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </>
